e78961169ccd54a9a93470ff1182d864208de82d
lianawadhwani <89544220+lianawadhwani@users.noreply.github.com>
2022-01-24 09:28:51 -0800

    did the same motion using save and restore instead

--- public/script.js
+++ public/script.js
@@ -119,21 +119,29 @@ canvas.width=window.screen.width;
 canvas.height=window.screen.height;
 var context= canvas.getContext("2d");
 
-var x=100;
+var x=.25;
 var y=20;
-var xstep= 1;
+var xstep=.25;
 var ystep=.25;
 window.requestAnimationFrame(function loop(){
 context.clearRect(0,0, canvas.width, canvas.height);
-if (x>innerWidth || x<0){
+if (x>200 || y>200 || x<0 || y<0){
     xstep=-xstep;
     ystep=-ystep;
   }
   x+=xstep;
   y+=ystep;
+context.save();
+context.translate(x,0);
+console.log(x);
 context.fillStyle="pink";
-context.fillRect(x,0, 100, 100);
+context.fillRect(0,0, 100, 100);
+context.restore();
+context.save();
+context.translate(0,y);
+console.log(y);
 context.fillStyle="blue";
-context.fillRect(0,y,100, 100)
+context.fillRect(0,0,100, 100);
+context.restore();
 window.requestAnimationFrame(loop);
 });
